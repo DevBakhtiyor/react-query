e1c5ef39392983f02276f0a8f3aba68c08cab991
DevBakhtiyor <bakhtiyor.dev.it@gmail.com>
2023-05-25 21:37:24 -0400

    Error Handling

--- src/Items.jsx
+++ src/Items.jsx
@@ -2,7 +2,7 @@ import { useQuery } from '@tanstack/react-query';
 import SingleItem from './SingleItem';
 import customFetch from './utils';
 const Items = ({ items }) => {
-  const { isLoading, data } = useQuery({
+  const { isLoading, data, isError, error } = useQuery({
     queryKey: ['tasks'],
     queryFn: async () => {
       const { data } = await customFetch.get('/');
@@ -13,6 +13,13 @@ const Items = ({ items }) => {
   if (isLoading) {
     return <p style={{ marginTop: '1rem' }}>Loading...</p>;
   }
+  if (isError) {
+    return <p style={{ marginTop: '1rem' }}>There was an error...</p>;
+  }
+  // console.log(error);
+  // if (error) {
+  //   return <p style={{ marginTop: '1rem' }}>{error.response.data}</p>;
+  // }
 
   return (
     <div className="items">
