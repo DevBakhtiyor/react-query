ab9abc5b4ba63f32cd88547101a696014da88dbf
DevBakhtiyor <bakhtiyor.dev.it@gmail.com>
2023-05-25 21:26:46 -0400

    Render Data

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import 'react-toastify/dist/ReactToastify.css';
-import {
-  QueryClient,
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const queryClient = new QueryClient();
 
